6950e2cf49903fa5655bb10ef0d03930504f0d23
Romain Roy <rroy.romain@gmail.com>
2019-10-27 21:17:50 +0100

    Config files

--- bot.js
+++ bot.js
@@ -1,18 +1,22 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 
 const client = new Discord.Client();
 
-const file = JSON.parse(fs.readFileSync('keys.json').toString());
-client.login(file.keys[0]);
-// client.login(process.env.TOKEN);
+const { token } = require('./config.json');
+// const token = client.login(process.env.TOKEN);
+
+client.login(token);
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
+
+	if (msg.author.bot) return;
+
 	if (msg.content === "ping") {
 		msg.channel.send("pong");
 	}
+
 });
