684e27eef750ce23b24343c0fd89f26c2e01d2f3
Romain Roy <rroy.romain@gmail.com>
2019-12-15 23:14:22 +0100

    Add music

--- bot.js
+++ bot.js
@@ -1,17 +1,18 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+// const { TOKEN } = require('./config.json');
+const { greetings } = require('./data.json');
 
-// const { token } = require('./config.json');
-const token = client.login(process.env.TOKEN);
+const client = new Discord.Client();
 
 const prefix = '!';
 
-const { greetings } = require('./data.json');
-
-client.login(token);
+var isPlaying = false;
+var voiceChannel, dispatcher;
 
 /* Initialisation du bot */
+
+client.login(process.env.TOKEN);
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -32,6 +33,71 @@ client.on('message', message => {
 
 	if (command === "beep") {
 		message.reply("boop");
+	}
+
+	if (command === "embed") {
+		let embedMessage = new Discord.RichEmbed()
+			.setColor("#0099ff")
+			.setTitle(message.author.username)
+			.setURL(message.author.avatarURL)
+			.setAuthor(client.user.username, client.user.avatarURL, client.user.avatarURL)
+			.setDescription(message.author.username + "'s properties.")
+			.addField("Status", message.author.presence.game.state)
+			.addField("Tag", message.author.tag)
+			.setThumbnail(message.author.avatarURL)
+			.setTimestamp()
+			.setFooter(client.user.username, client.user.avatarURL);
+		message.channel.send(embedMessage);
+	}
+
+	if (command === "play") {
+		if (!isPlaying) {
+			isPlaying = true;
+			voiceChannel = message.member.voiceChannel;
+			try {
+				voiceChannel.join().then(connection => {
+					try {
+						dispatcher = connection.playArbitraryInput(args[0]);
+						dispatcher.on('end', () => {
+							connection.disconnect();
+							isPlaying = false;
+						});
+					} catch (e) {
+						message.reply("il m'est malheuresement impossible de lire cette musique.");
+						connection.disconnect();
+						isPlaying = false;
+					}
+				});
+			} catch (e) {
+				message.reply("je regrette, vous n'êtes pas en vocal.");
+				isPlaying = false;
+			}
+		} else {
+			message.reply("je suis déjà occupé. Vous pouvez arrêter ma tâche précédente avec `!stop`.");
+		}
+	}
+
+	if (command === "volume" && isPlaying) {
+		if (args[0] >= 0 && args[0] <= 100) {
+			message.channel.send("Volume set to " + args[0] + "%.");
+			dispatcher.setVolume(args[0] / 100);
+		} else {
+			message.channel.send("Volume must be between 0 and 100.");
+		}
+	}
+
+	if (command === "pause" && isPlaying) {
+		dispatcher.pause();
+	}
+
+	if (command === "resume" && isPlaying) {
+		dispatcher.resume();
+	}
+
+	if (command === "stop" && isPlaying) {
+		dispatcher.end();
+		voiceChannel.leave();
+		isPlaying = false;
 	}
 
 });
